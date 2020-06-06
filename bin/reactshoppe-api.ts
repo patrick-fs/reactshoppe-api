81df8ae0f02efea22cd638b339a13cc5b80dc7e0
Patrick Brandt <patrick@fullstory.com>
2020-06-05 20:42:13 -0400

    cdk init --language typescript

---
+++ bin/reactshoppe-api.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { ReactshoppeApiStack } from '../lib/reactshoppe-api-stack';
+
+const app = new cdk.App();
+new ReactshoppeApiStack(app, 'ReactshoppeApiStack');
