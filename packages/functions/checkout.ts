8d56807e04e983586cfe15ee683d288b0d4b0f15
Patrick Brandt <patrick@fullstory.com>
2020-10-01 11:39:25 -0400

    storing billing info and products from the request

--- packages/functions/checkout.ts
+++ packages/functions/checkout.ts
@@ -6,23 +6,29 @@ import { success, failure } from './response';
 
 export default async (event: APIGatewayProxyEvent) => {
   const ddb = new AWS.DynamoDB.DocumentClient();
-  const newOrder = {
-    id: uuidv4(),
-    products: ['1', '2', '3'],
-  };
-  try {
-    const result = await ddb.put({
-      TableName: TableNames.Order,
-      Item: newOrder
-    }).promise();
-    
-    const body = JSON.stringify(result.$response.data);
-    console.log(`successful ddb request: ${body}`);
-
-    return success(body);
-
-  } catch (error) {
-    const body = error.stack || JSON.stringify(error, null, 2);
-    return failure(body);
+  let newOrder = {};
+  if (event.body != null) {
+    const body = JSON.parse(event.body);
+    newOrder = {
+      id: uuidv4(),
+      products: body.products,
+      billingInfo: body.billingInfo,
+    };
+    try {
+      const result = await ddb.put({
+        TableName: TableNames.Order,
+        Item: newOrder
+      }).promise();
+      
+      const body = JSON.stringify(result.$response.data);
+      console.log(`successful ddb request: ${body}`);
+  
+      return success(body);
+  
+    } catch (error) {
+      const body = error.stack || JSON.stringify(error, null, 2);
+      return failure(body);
+    }
   }
+  return success(JSON.stringify(newOrder));
 };
