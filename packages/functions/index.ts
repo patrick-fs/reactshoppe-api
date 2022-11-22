5b5f99624ede89424c40a3db45017d75db06cc3c
Patrick Brandt <patrick@fullstory.com>
2022-11-21 19:57:05 -0500

    better error handling in case NewRelic agent throws an exception

--- packages/functions/index.ts
+++ packages/functions/index.ts
@@ -5,16 +5,22 @@ import checkout from './checkout';
 import { failure, fullstoryHeader } from './response';
 
 exports.main = async (event: APIGatewayProxyEvent) => {
-  const fullstoryURL = event.headers ? event.headers[fullstoryHeader.toLocaleLowerCase()] : undefined;
-  addCustomAttribute('FullStoryURL', fullstoryURL || 'none provided');
-  const notFound = () => failure(JSON.stringify({ message: 'not found' }), 404);
-  switch (event.path) {
-    case '/ping':
-      return ping();
-    case '/checkout':
-      if (event.httpMethod === 'POST') return checkout(event);
-      return notFound();
-    default:
-      return notFound();
+  try {
+    const fullstoryURL = event.headers ? event.headers[fullstoryHeader.toLocaleLowerCase()] : undefined;
+    addCustomAttribute('FullStoryURL', fullstoryURL || 'none provided');
+    const notFound = () => failure(JSON.stringify({ message: 'not found' }), 404);
+    switch (event.path) {
+      case '/ping':
+        return ping();
+      case '/checkout':
+        if (event.httpMethod === 'POST') return checkout(event);
+        return notFound();
+      default:
+        return notFound();
+    }
+  } catch(error) {
+    const e = error as Error;
+    return failure(e.message);
   }
+  
 };
